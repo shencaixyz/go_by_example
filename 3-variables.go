7a3715ecf701ebcc54ed5d193eca3738069ee05f
沈财 <1264010730@qq.com>
2022-01-11 09:07:55 +0800

    变量

--- 3-variables.go
+++ 3-variables.go
@@ -12,4 +12,14 @@ func main() {
 	fmt.Println(b, c)
 
 	//字段推断以及初始化的变量类型
+	var d = true
+	fmt.Println(d)
+
+	//声明后却没有给出初始值 则变量会初始化未零
+	var e int
+	fmt.Println(e)
+
+	// := 语法时声明并初始化变量的简写
+	f := "short"
+	fmt.Println(f)
 }
