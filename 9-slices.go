28d334a75ff12f3cc972ae764e446a965b1a88d9
沈财 <1264010730@qq.com>
2022-01-11 14:18:15 +0800

    切片

---
+++ 9-slices.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+func main() {
+	//
+	s := make([]string, 3)
+	fmt.Println("emp:", s)
+
+	s[0] = "a"
+	s[1] = "b"
+	s[2] = "c"
+	fmt.Println("set:", s)
+	fmt.Println("get:", s[2])
+
+	fmt.Println("len:", len(s))
+
+	s = append(s, "d")
+	s = append(s, "e", "f")
+	fmt.Println("apd:", s)
+
+	c := make([]string, len(s))
+	copy(c, s)
+	fmt.Println("cpy:", c)
+
+	//切片
+	l := s[2:5]
+	fmt.Println("sl1:", l)
+
+	l = s[:5]
+	fmt.Println("sl2:", l)
+
+	l = s[2:]
+	fmt.Println("sl3:", l)
+
+	//声明并初始化slice变量
+	t := []string{"g", "h", "i"}
+	fmt.Println("dcl:", t)
+
+	//slice 可以组成多维数据结构
+	//内部的 slice 长度可以不一致 这和多维数组
+	twoD := make([][]int, 3)
+	for i := 0; i < 3; i++ {
+		innerLen := i + 1
+		twoD[i] = make([]int, innerLen)
+		for j := 0; j < innerLen; j++ {
+			twoD[i][j] = i + j
+		}
+	}
+	fmt.Println("2d: ", twoD)
+}
