7e1d00d0d5aee9878b946689be34c6e986946591
shencai <1264010730@qq.com>
2022-03-09 17:42:16 +0800

    结构体

---
+++ 18-structs.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+type person struct {
+	name string
+	age  int
+}
+
+func main() {
+	fmt.Println(person{"Bob", 20})
+
+	fmt.Println(person{name: "Alice", age: 30})
+
+	fmt.Println(person{name: "销售"})
+}
